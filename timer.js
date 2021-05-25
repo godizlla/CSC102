00f051e191671c3b2b31c23f0374e6b9f2c2b198
godizlla <licksomebooty@gmail.com>
2021-05-24 22:09:14 -0700

    Add files via upload

--- timer.js
+++ timer.js
@@ -38,3 +38,24 @@ function countdowntimer(){
     currTime = currTime - 5;
     }, 50000);
 }
+
+
+/* Dice Roll  */
+
+function playcraps(){
+    //For the consloe to log
+    console.log("Craps Game Started")
+    var die1 = Math.ceil(Math.random() * 6);
+    var die2 = Math.ceil(Math.random() * 6);
+    document.getElementById("die1Res").innerHTML = die1;
+    document.getElementById("die2Res").innerHTML = die2;
+    var dieSum = die1 + die2;
+    document.getElementById("diceSum").innerHTML = dieSum;
+    if(dieSum == 7 || dieSum == 11){
+        document.getElementById("crapsStatus").innerHTML = "Craps! You Lose";
+    }else if (die1 == die2 && die1 % 2 ==0){
+        document.getElementById("crapsStatus").innerHTML = "Double You Win";
+    }else {
+        document.getElementById("crapsStatus").innerHTML = "You didn't win or lose roll again"
+    }
+}
